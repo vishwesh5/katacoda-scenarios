353f79ca26b50035938fa50ae281f76cbcb91dc9
vishwesh5 <vishweshshrimali5@gmail.com>
2020-08-16 19:57:21 +0530

    Add data download instructions

--- handling-imbalanced-classes/run-python3.sh
+++ handling-imbalanced-classes/run-python3.sh
@@ -1,2 +1,5 @@
 #/bin/bash
+# Download the data
+wget https://raw.githubusercontent.com/vishwesh5/katacoda-scenarios/master/handling-imbalanced-classes/data.csv -O data.csv
+# Start Python3
 python3
